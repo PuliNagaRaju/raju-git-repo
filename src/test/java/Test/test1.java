53cc6129cc79d844fe6081ec0496e524a4e63ffd
Naga Raju <puli.nagaraju1219@gamil.com>
2023-05-17 23:24:15 +0800

    test1

--- src/test/java/Test/test1.java
+++ src/test/java/Test/test1.java
@@ -1,11 +1,10 @@
 package Test;
 
-public class WelcomeWorld {
+public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("helo world");
-		System.out.println("helo world");
+System.out.println("test1");
 	}
 
 }
